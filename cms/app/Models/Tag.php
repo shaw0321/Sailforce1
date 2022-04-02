7de4f40579ddbfdcd2893cdfd08a46e94050082d
shaw0321 <ikebeshaw0321@gmail.com>
2022-04-02 14:48:15 +0000

    tagを追加

---
+++ cms/app/Models/Tag.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+    
+    protected $primaryKey = 'tag_id';
+    
+    // Userテーブルとの多対多リレーション
+     public function tag_user() {
+        return $this->belongsToMany('App\Models\User');
+    }
+    
+        // Userテーブルとの多対多リレーション
+     public function tag_post() {
+        return $this->belongsToMany('App\Models\Post');
+    }
+}
---
+++ cms/database/seeders/TagTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    \App\Models\Tag::factory(100)->create();
+    }
+}
